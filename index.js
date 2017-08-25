ab7eedc165c0dbec4c9dfd03528a0544c77c9b9d
mjohnston@brainstation.io <mjohnston@konradgroup.com>
2017-08-25 10:01:33 -0400

    add callback route

--- index.js
+++ index.js
@@ -61,6 +61,9 @@ app.use(express.static(path.resolve(__dirname, 'build')));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+app.get('/callback', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
