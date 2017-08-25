b317cadee3e64778834b314a423cc45f26e61e24
mjohnston@brainstation.io <mjohnston@konradgroup.com>
2017-08-24 23:26:45 -0400

    use real login

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 const AUTH_CONFIG = {
   domain: 'marcjohnstonuw.auth0.com',
   clientId: 'sO78Oq2NopXAHukBwD3rnfQbnwYWVHF4',
-  callbackUrl: 'http://localhost:9000/callback'
+  callbackUrl: 'https://bingo-gen.herokuapp.com/callback'
 }
 
 export default AUTH_CONFIG;
